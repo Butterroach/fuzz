7b96cdf633401b8d2ab83845c0d094ba841acf82
Butterroach <butterroach@proton.me>
2023-10-01 20:12:16 +0300

    add a shell ig?

--- interpreter.py
+++ interpreter.py
@@ -1,17 +1,13 @@
 import sys
 
+VERSION = "1.0.0"
+
 
 class FuzzError(Exception):
     pass
 
 
-if not sys.argv[1].endswith(".fuzz"):
-    raise FuzzError()
-
-with open(sys.argv[1]) as f:
-    lines = f.read().splitlines()
-
-for line in lines:
+def interpret(line: str):
     if line.startswith("echo "):
         if line.endswith(";"):
             line = line[::-1].replace(";", "", 1)[
@@ -22,5 +18,31 @@ for line in lines:
             raise FuzzError()
     else:
         if line.startswith("//"):
-            continue  # AT LEAST I ADDED COMMENTS (although you can't put them at the end of lines)
+            return  # AT LEAST I ADDED COMMENTS (although you can't put them at the end of lines)
         raise FuzzError()
+
+
+if len(sys.argv) > 1:
+    if not sys.argv[1].endswith(".fuzz"):
+        raise FuzzError()
+
+    with open(sys.argv[1]) as f:
+        lines = f.read().splitlines()
+
+    for line in lines:
+        interpret(line)
+else:
+    print(f"fuzz v{VERSION} shell. press ctrl+c to close.")
+    while True:
+        try:
+            line = input("fuzz> ")
+            interpret(line)
+        except FuzzError:
+            print("FuzzError occurred")
+            print("you messed up and i wont tell you!!!!")
+            continue
+        except (KeyboardInterrupt, EOFError):
+            break
+
+print("\nfinished executing!")
+sys.exit(0)
